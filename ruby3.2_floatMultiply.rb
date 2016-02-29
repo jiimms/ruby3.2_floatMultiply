3d72465662f89618c513aa1a42dadc0c859714bc
jiimms <jiimms@yahoo.com>
2016-03-01 01:13:12 +0530

    ruby 3.2

---
+++ ruby3.2_floatMultiply.rb
@@ -0,0 +1,17 @@
+
+	def multiply()
+		puts "Please enter first value"
+		first = gets.to_f
+
+		puts "Please enter second value"
+		second = gets.to_f
+
+
+		puts first * second
+		
+	end
+
+	multiply()
+	
+	
+		
